e6c941f2fd768e1fe738e09db63ce4abe8cf9baf
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-07-11 00:53:30 +1200

    fixed leak

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
 
+// HTTP logging
+app.use(morgan("dev"));
+
 app.listen(PORT, () => {
   console.log(`Server is live and listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  console.log("Hello");
+  res.status(200).send("OK");
 });
