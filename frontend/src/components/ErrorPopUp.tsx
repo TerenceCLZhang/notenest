d0443b885c16116bc01dee575538c0e21395a144
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-07-19 02:02:10 +1200

    styles

--- frontend/src/components/ErrorPopUp.tsx
+++ frontend/src/components/ErrorPopUp.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const ErrorPopup = ({ text, onClose }: Props) => {
   return (
-    <div className="fixed left-1/2 -translate-x-1/2 top-10 flex items-center gap-5 bg-red-800 text-white p-5 rounded-lg shadow-lg w-[85%] md:max-w-[50%]">
+    <div className="fixed left-1/2 -translate-x-1/2 top-10 flex items-center gap-5 bg-red-800 text-white p-5 rounded-lg shadow-lg w-[85%] md:w-fit z-5">
       {text}
       <button type="button" onClick={onClose}>
         <FontAwesomeIcon icon={faXmark} />
